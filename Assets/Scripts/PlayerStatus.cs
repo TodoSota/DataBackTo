e15ffcec6386405f8dde9e6f871b2ad7c1f31a1e
TodoSota <137256865+TodoSota@users.noreply.github.com>
2026-01-22 14:41:44 +0900

    [add]Playr関連クラス
    Player属性を持つ、Statusクラス
    Player属性を扱う、Controllerクラス
    を追加

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    // 基本ステータス
+    public float hp = 100f;
+    public int money = 0;
+    public const int MaxMoney = 50;
+
+    // 自動減少値
+    public float hpLossPerSecond = 0.5f;
+
+    // ジャンプ管理
+    public int currentJumpCount = 0;
+    public int maxJumpLimit = 1;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(rb.velocity.x) > 0.1f)
+        {
+            ConsumeHp(hpLossPerSecond * Time.deltaTime);
+        }
+
+        if (hp <= 0)
+        {
+            Die();
+        }
+
+        UnityEngine.Debug.Log("Hp : " + hp);
+        UnityEngine.Debug.Log("JumpCount : " + currentJumpCount);
+    }
+
+    public void ConsumeHp(float amount)
+    {
+        hp -= amount;
+        hp = Mathf.Max(hp, 0);
+    }
+
+    public void AddMoney(int amount)
+    {
+        money += amount;
+        money = Mathf.Min(money, MaxMoney);
+    }
+
+    void Die()
+    {
+        UnityEngine.Debug.Log("Died!!");
+    }
+
+    public void ResetJumpConut()
+    {
+        currentJumpCount = 0;
+    }
+}
