047fec9df6d05a1bc959cf46e98f1cab69bc7cff
otukebo-ya <136353469+otukebo-ya@users.noreply.github.com>
2026-04-14 10:03:04 +0900

    Revert "プレイヤーの物理の適用On/Offの修正"

--- Assets/Scripts/StageScripts/Player/PlayerController.cs
+++ Assets/Scripts/StageScripts/Player/PlayerController.cs
@@ -20,7 +20,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float defaultJumpForce = 5.0f;
     [SerializeField] private float ShortJumpRate = 0.8f;
     [SerializeField] private float BurnJumpRate = 1.5f;
-
     // ジャンプ力のプロパティ
     public float jumpForce => (status.CurrentCondition == PlayerCondition.Short)? defaultJumpForce * ShortJumpRate :
                                (status.CurrentCondition == PlayerCondition.Burn)? defaultJumpForce * BurnJumpRate :
@@ -119,17 +118,6 @@ public class PlayerController : MonoBehaviour
         rb.velocity = Vector3.zero;
     }
 
-    public void EnablePhysics()
-    {
-        Stop();
-        rb.isKinematic = true;
-    }
-
-    public void DisablePhysics()
-    {
-        rb.isKinematic = false;
-    }
-
     void OnPlayerDeath()
     {
         rb.isKinematic = true;
@@ -137,7 +125,7 @@ public class PlayerController : MonoBehaviour
 
     void SetUp()
     {
-        DisablePhysics();
+        rb.isKinematic = false;
         Stop();
     }
 
